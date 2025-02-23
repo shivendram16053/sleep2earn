1cf1097a31b0ea183dc0b63780d1395ef7c9c7b6
hariFED <109363771+hariFED@users.noreply.github.com>
2025-02-23 14:25:40 +0530

    add

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -50,34 +50,34 @@ const Navbar = () => {
         <Link href="/trade"><p style={{ margin: "0 1rem" }}>Trade</p></Link>
       </div>
 
-        {/* Navigation Links - Hidden on Small Screens */}
-        <div className="hidden md:flex">
-          <ul className="flex items-center gap-4">
-            {["Directory", "Resources", "Solutions"].map((item) => (
-              <li key={item}>
-                <button className="group cursor-pointer inline-flex h-10 items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-white/10 focus:bg-white/10 focus:outline-none disabled:pointer-events-none disabled:opacity-50">
-                  {item}
-                </button>
-              </li>
-            ))}
-            {[
-              { name: "Blog", href: "/blog" },
-              { name: "Careers", href: "/careers" },
-            ].map((link) => (
-              <li key={link.name}>
-                <Link
-                  href={link.href}
-                  className="group inline-flex h-10 cursor-pointer  items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-white/10 focus:bg-white/10 focus:outline-none disabled:pointer-events-none disabled:opacity-50"
-                >
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+      {/* Navigation Links - Hidden on Small Screens */}
+      <div className="hidden md:flex">
+        <ul className="flex items-center gap-4">
+          {["Directory", "Resources", "Solutions"].map((item) => (
+            <li key={item}>
+              <button className="group cursor-pointer inline-flex h-10 items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-white/10 focus:bg-white/10 focus:outline-none disabled:pointer-events-none disabled:opacity-50">
+                {item}
+              </button>
+            </li>
+          ))}
+          {[
+            { name: "Blog", href: "/blog" },
+            { name: "Careers", href: "/careers" },
+          ].map((link) => (
+            <li key={link.name}>
+              <Link
+                href={link.href}
+                className="group inline-flex h-10 cursor-pointer  items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-white/10 focus:bg-white/10 focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
 
 
-      </nav>
-    </header>
+    </nav>
+
   );
 }
